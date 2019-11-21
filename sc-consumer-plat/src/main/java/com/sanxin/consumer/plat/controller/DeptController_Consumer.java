c1c97166de3f02a580a1d6dc732cc7f931d9eef9
changys <654002130@qq.com>
2019-11-21 17:41:45 +0800

    213

--- sc-consumer-plat/src/main/java/com/sanxin/consumer/plat/controller/DeptController_Consumer.java
+++ sc-consumer-plat/src/main/java/com/sanxin/consumer/plat/controller/DeptController_Consumer.java
@@ -35,9 +35,6 @@ public class DeptController_Consumer {
 
     @RequestMapping(value = "/consumer/dept/discovery")
     public Object discovery() {
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
     }
 }
